4af80834dbeca46abc5bd44c654fb80fbe2cd5e7
Cory Pease <coryrpease@gmail.com>
2018-08-25 10:02:06 -0600

    screen: numbers & operator display, clear functions

--- js/index.js
+++ js/index.js
@@ -1,33 +1,45 @@
+// VARIABLES
+let screen = document.querySelector('#screen'),
+    allButtons = document.querySelectorAll('span'),
+    operators = document.querySelectorAll('.operator'),
+    clear = document.querySelector('#clear'),
+    zero = document.querySelector('#zero'),
+    equals = document.querySelector('#equals'),
+    firstNumber,
+    operator,
+    secondNumber,
+    totalvalue;
+
+// FUNCTIONS
+function numbersDo(numberElement) {
+    numberElement.addEventListener('click', function () {
+        if (numberElement.textContent === '0' && screen.textContent === '') {
+            alert('Starting with nothing is a hard road! i am in numbersDo')
+        } else {
+            screen.textContent += numberElement.textContent
+        }
+    });
+};
+
+function operatorsDo(operatorElement) {
+    operatorElement.addEventListener('click', function () {
+        if (screen.textContent === '') {
+            alert('Starting with nothing is a hard road! i am in operators do')
+        } else {
+            screen.textContent += operatorElement.textContent
+        }
+    });
+};
+
+function clearDoes(clearElement) {
+    clearElement.addEventListener('click', function () {
+        screen.textContent = ''
+    });
+};
+
 document.addEventListener('DOMContentLoaded', function () {
 
-    // VARIABLES
-    const screen = document.querySelector('#screen');
-    const allButtons = document.querySelectorAll('span');
-    const operators = document.querySelectorAll('.operator');
-    const clear = document.querySelector('#clear');
-    const zero = document.querySelector('#zero');
-    const equals = document.querySelector('#equals');
-
-    // FUNCTIONS
-    function numbersDo(numberElement) {
-        numberElement.addEventListener('click', function () {
-            screen.textContent += numberElement.textContent 
-        });
-    };
-
-    function operatorsDo(operatorElement) {
-        operatorElement.addEventListener('click', function () {
-            screen.textContent += operatorElement.textContent
-        });
-    };
-
-    function clearDoes(clearElement) {
-        clearElement.addEventListener('click', function () {
-            screen.textContent = ''    
-        });
-    };
-
-    // LOGIC
+    // Main Invocation
     allButtons.forEach(element => {
         if (element === clear) {
             // console.log('i am clear', element)
