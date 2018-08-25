3f036ae3ee6a9638c7d810338225e49ef1ade7d2
Cory Pease <coryrpease@gmail.com>
2018-08-25 17:03:48 -0600

    calc can not handle multiple operations, current bug - if operator hit twice calc breaks

--- js/index.js
+++ js/index.js
@@ -7,27 +7,68 @@ let screen = document.querySelector('#screen'),
     equals = document.querySelector('#equals'),
     firstNumber,
     mathSymbol,
-    secondNumber,
-// FUNCTIONS
+    screenLength,
+    secondNumber;
 
-function clearAll() {
-    screen.textContent = ''
-    firstNumber = undefined
-    mathSymbol = undefined
-    secondNumber = undefined
-}
+// Main Invocation
+document.addEventListener('DOMContentLoaded', function () {
+    allButtons.forEach(element => {
+        if (element === clear) {
+            clearDoes(element);
+        } else if (element.className === 'operator') {
+            operatorsDo(element);
+        } else {
+            numbersDo(element);
+        };
+    });
+});
+
+    // FUNCTIONS
 
 function numbersDo(numberElement) {
     numberElement.addEventListener('click', function () {
         if (numberElement.textContent === '0' && screen.textContent === '') {
-            alert('Starting with nothing is a hard road! i am in numbersDo')
+            screen.textContent = 'ERROR'
         } else {
             screen.textContent += numberElement.textContent
-        }
+        };
+    });
+};
+
+function operatorsDo(operatorElement) {
+    operatorElement.addEventListener('click', function () {
+        if (screen.textContent === '' || screen.textContent === 'ERROR') {
+            screen.textContent = 'ERROR'
+        } else if (operatorElement === equals && firstNumber) {
+            doMath();
+        } else if (screen.textContent === '0') {
+            alert('I CANNOT START WITH 0!')
+        } else if (firstNumber) {
+            doMath();
+            mathSymbol = operatorElement.textContent
+            screen.textContent += operatorElement.textContent
+            screenLength = screen.textContent.length
+        } else {            
+            firstNumber = parseInt(screen.textContent);
+            mathSymbol = operatorElement.textContent;
+            screen.textContent += operatorElement.textContent;
+            screenLength = screen.textContent.length
+        };
+    });
+};
+
+function clearDoes(clearElement) {
+    clearElement.addEventListener('click', function () {
+        screen.textContent = '';
+        firstNumber = undefined;
+        mathSymbol = undefined;
+        secondNumber = undefined;
     });
 };
 
 function doMath() {
+    secondNumber = parseInt(screen.textContent.slice(screenLength,))
+    console.log(secondNumber)
     switch (mathSymbol) {
         case 'x':
         firstNumber = firstNumber * secondNumber
@@ -41,45 +82,9 @@ function doMath() {
         case '-':
         firstNumber = firstNumber - secondNumber
             break;
-    }
-    secondNumber = undefined
-    mathSymbol = undefined
-    screen.textContent = firstNumber
-}
-
-function operatorsDo(operatorElement) {
-    operatorElement.addEventListener('click', function () {
-        if (firstNumber) {
-            secondNumber = parseInt(screen.textContent)
-            doMath()
-        } else {
-            firstNumber = parseInt(screen.textContent)
-            mathSymbol = operatorElement.textContent
-            screen.textContent = ''
-        }
-    });
+    };
+    secondNumber = undefined;
+    mathSymbol = undefined;
+    screen.textContent = firstNumber;
 };
 
-function clearDoes(clearElement) {
-    clearElement.addEventListener('click', function () {
-        clearAll()
-        console.log(firstNumber, mathSymbol, secondNumber);
-    });
-};
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    // Main Invocation
-    allButtons.forEach(element => {
-        if (element === clear) {
-            // console.log('i am clear', element)
-            clearDoes(element)
-        } else if (element.className === 'operator') {
-            // console.log('we are operators', element)
-            operatorsDo(element)
-        } else {
-            // console.log('we are numbers', element)
-            numbersDo(element)
-        }
-    });
-});
